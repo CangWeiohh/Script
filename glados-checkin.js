098a357059139b590666b860f4b70f551d7fe5b1
CangWei <96470348+CangWeiohh@users.noreply.github.com>
2023-04-14 09:06:22 +0800

    Update glados-checkin.js

--- glados-checkin.js
+++ glados-checkin.js
@@ -8,40 +8,52 @@
  * 0 9 * * * glados-checkin.js
  */
 
-const url = "https://glados.rocks/api/user/checkin";
-const headers = {
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh-Hans;q=0.9",
-    "Authorization": "5547625865797486976414591193354-900-1440", // 填写你的Authorization值
-    "Connection": "keep-alive",
-    "Content-Length": "26",
-    "Content-Type": "application/json;charset=utf-8",
-    "Cookie": "koa:sess=eyJ1c2VySWQiOjk5NDM1LCJfZXhwaXJlIjoxNzA3MzE3NTk0NTIzLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=A1rM7rt1GX2Slb_3Lg1PKOZOqRQ", // 填写你的 koa:sess 值
-    "Host": "glados.rocks",
-    "Origin": "https://glados.rocks",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15"
-};
+const checkin = (authorization, cookie) => {
+  const url = "https://glados.rocks/api/user/checkin";
+  const headers = {
+      "Accept": "application/json, text/plain, */*",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+      "Authorization": authorization,
+      "Connection": "keep-alive",
+      "Content-Length": "26",
+      "Content-Type": "application/json;charset=utf-8",
+      "Cookie": `koa:sess=${cookie}`,
+      "Host": "glados.rocks",
+      "Origin": "https://glados.rocks",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-origin",
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15"
+  };
+  
+  const body = {"token":"glados.network"};
+  
+  const myRequest = {
+      url: url,
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(body)
+  };
+  
+  $task.fetch(myRequest).then(response => {
+      const result = JSON.parse(response.body);
+      if (result.code === 0) {
+          $notify("GLaDOS签到", "签到成功", `获得 ${result.list[0]} 天奖励`);
+      } else {
+          $notify("GLaDOS签到", "签到失败", `错误信息：${result.message}`);
+      }
+  }, reason => {
+      $notify("GLaDOS签到", "请求失败", reason.error);
+  }); 
+}
 
-const body = {"token":"glados.network"};
+// 第一个账号
+const authorization1 = "5547625865797486976414591193354-900-1440";
+const cookie1 = "_ga=GA1.2.373731192.1673831281; koa:sess=eyJ1c2VySWQiOjkxMTI2LCJfZXhwaXJlIjoxNjk2NjUxMTQzNDc2LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=wenYvhH7vx3XPSVNfAzTjUVzbVo";
+checkin(authorization1, cookie1);
 
-const myRequest = {
-    url: url,
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(body)
-};
-
-$task.fetch(myRequest).then(response => {
-    const result = JSON.parse(response.body);
-    if (result.code === 0) {
-        $notify("GLaDOS签到", "签到成功", `获得 ${result.list[0]} 天奖励`);
-    } else {
-        $notify("GLaDOS签到", "签到失败", `错误信息：${result.message}`);
-    }
-}, reason => {
-    $notify("GLaDOS签到", "请求失败", reason.error);
-}); 
+// 第二个账号
+const authorization2 = "5547625865797486976414591193354-900-1440";
+const cookie2 = "koa:sess=eyJ1c2VySWQiOjk5NDM1LCJfZXhwaXJlIjoxNzA3MzE3NTk0NTIzLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=A1rM7rt1GX2Slb_3Lg1PKOZOqRQ";
+checkin(authorization2, cookie2);
