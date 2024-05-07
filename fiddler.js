20d8838361e72bd637e8e8ef191f2e8ad68c537d
CangWei <96470348+CangWeiohh@users.noreply.github.com>
2024-05-07 10:35:39 +0800

    Update fiddler.js

--- fiddler.js
+++ fiddler.js
@@ -1,11 +1,11 @@
 let body = JSON.parse($response.body);
 body = JSON.stringify({
-  "id" : "8050ffd5-9ff1-4e02-bea7-9226d4d0ec00",
+  "id" : "de877622-e99c-4812-b8ca-4c8528f0a3d0",
   "disabledAnalyticsTracking" : false,
   "lastName" : "Cang",
   "unreadEvents" : 0,
   "firstName" : "Wei",
-  "internal" : false,
+  "internal" : true,
   "createdAt" : "2024-05-07T02:08:26.5996500Z",
   "modifiedAt" : "2024-05-07T02:08:26.5996500Z",
   "accounts" : [
@@ -14,7 +14,7 @@ body = JSON.stringify({
   "isJamTrialUsed" : false,
   "seenGettingStartedInJam" : false,
   "isEverywhereTrialUsed" : false,
-  "email" : "cangweiohh@foxmail.com",
+  "email" : "cangweiohhi@gmail.com",
   "name" : "Wei Cang",
   "incomplete" : false
 })
